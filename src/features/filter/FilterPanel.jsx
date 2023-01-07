53ccaec00d99406680da363d08dfbc2b238d9b2d
Bohdan Karpinskyi <bohdan2395@gmail.com>
2023-01-07 13:15:17 +0200

    first commit

---
+++ src/features/filter/FilterPanel.jsx
@@ -0,0 +1,39 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
+import { Badge } from 'UI/Badge';
+import { Card } from 'UI/Card';
+import { Stack } from 'UI/Stack';
+
+import { removeFilter, clearFilter, selectFilters } from './filter-slice';
+
+const FilterPanel = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+
+  if (currentFilters.length === 0) return null;
+
+  return (
+    <Card className="filter-panel">
+      <div className="filter-panel-wrapper">
+        <Stack>
+          {currentFilters.map((filter) => (
+            <Badge
+              variant="clearable"
+              key={filter}
+              onClear={() => dispatch(removeFilter(filter))}
+            >
+              {filter}
+            </Badge>
+          ))}
+        </Stack>
+
+        <button className="link" onClick={() => dispatch(clearFilter())}>
+          Clear
+        </button>
+      </div>
+    </Card>
+  );
+};
+
+export { FilterPanel };
